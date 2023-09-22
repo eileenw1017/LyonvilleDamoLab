271e6c620bc77ce1b0f2407fdd9e4fb068d51896
eileenw1017 <rouyingw@student.unimelb.edu.au>
2023-09-22 16:03:28 +1000

    History
    history UI first time to push

---
+++ History/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { MacBookPro1613 } from "./MacBookPro1613/MacBookPro1613";
+
+export default function App() {
+return (
+    <div>
+    <MacBookPro1613 />
+    </div>
+);
+}
---
+++ History/src/MacBookPro1613/MacBookPro1613.jsx
@@ -0,0 +1,255 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import styles from "./MacBookPro1613.module.css";
+
+export const MacBookPro1613 = ({ ...props }) => {
+  return (
+    <div className={styles["body"]}>
+      <div className={styles["header"]}>
+        <div className={styles["header-content"]}>
+          <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="59" height="55" viewBox="0 0 59 55" fill="none">
+              <g filter="url(#filter0_d_1284_17527)">
+                <ellipse cx="29.1622" cy="23.5" rx="25.0431" ry="23.5" fill="#ACB594"/>
+              </g>
+              <path d="M40.081 13C40.081 13.5121 39.7954 14.013 39.2499 14.4811C38.7048 14.9489 37.9102 15.3748 36.9195 15.7344C34.939 16.4534 32.1963 16.9 29.162 16.9C26.1277 16.9 23.3851 16.4534 21.4046 15.7344C20.4139 15.3748 19.6192 14.9489 19.0741 14.4811C18.5287 14.013 18.2431 13.5121 18.2431 13C18.2431 12.4879 18.5287 11.987 19.0741 11.5189C19.6192 11.0511 20.4139 10.6252 21.4046 10.2656C23.3851 9.54663 26.1277 9.1 29.162 9.1C32.1963 9.1 34.939 9.54663 36.9195 10.2656C37.9102 10.6252 38.7048 11.0511 39.2499 11.5189C39.7954 11.987 40.081 12.4879 40.081 13Z" fill="#414730" stroke="black" stroke-width="0.2"/>
+              <path d="M40.081 23C40.081 23.5121 39.7954 24.013 39.2499 24.4811C38.7048 24.9489 37.9102 25.3748 36.9195 25.7344C34.939 26.4534 32.1963 26.9 29.162 26.9C26.1277 26.9 23.3851 26.4534 21.4046 25.7344C20.4139 25.3748 19.6192 24.9489 19.0741 24.4811C18.5287 24.013 18.2431 23.5121 18.2431 23C18.2431 22.4879 18.5287 21.987 19.0741 21.5189C19.6192 21.0511 20.4139 20.6252 21.4046 20.2656C23.3851 19.5466 26.1277 19.1 29.162 19.1C32.1963 19.1 34.939 19.5466 36.9195 20.2656C37.9102 20.6252 38.7048 21.0511 39.2499 21.5189C39.7954 21.987 40.081 22.4879 40.081 23Z" fill="#414730" stroke="black" stroke-width="0.2"/>
+              <path d="M40.081 33C40.081 33.5121 39.7954 34.013 39.2499 34.4811C38.7048 34.9489 37.9102 35.3748 36.9195 35.7344C34.939 36.4534 32.1963 36.9 29.162 36.9C26.1277 36.9 23.3851 36.4534 21.4046 35.7344C20.4139 35.3748 19.6192 34.9489 19.0741 34.4811C18.5287 34.013 18.2431 33.5121 18.2431 33C18.2431 32.4879 18.5287 31.987 19.0741 31.5189C19.6192 31.0511 20.4139 30.6252 21.4046 30.2656C23.3851 29.5466 26.1277 29.1 29.162 29.1C32.1963 29.1 34.939 29.5466 36.9195 30.2656C37.9102 30.6252 38.7048 31.0511 39.2499 31.5189C39.7954 31.987 40.081 32.4879 40.081 33Z" fill="#414730" stroke="black" stroke-width="0.2"/>
+              <defs>
+                <filter id="filter0_d_1284_17527" x="0.119141" y="0" width="58.0864" height="55" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                  <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                  <feOffset dy="4"/>
+                  <feGaussianBlur stdDeviation="2"/>
+                  <feComposite in2="hardAlpha" operator="out"/>
+                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1284_17527"/>
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1284_17527" result="shape"/>
+                </filter>
+              </defs>
+            </svg>
+          </div>
+          <div className={styles["menu"]}>
+            <div className={styles["menu-item"]}>
+              <a href="#">About Lyonville</a>
+              <a href="#" className={styles["small"]}>Feedback and Ideas</a>
+              <a href="#" className={styles["small"]}>Facebook</a>
+            </div>
+            <div className={styles["menu-item"]}>
+              <a href="#">Getting There</a>
+              <a href="#" className={styles["small"]}>Real Estate</a>
+              <a href="#" className={styles["small"]}>Flora and Fauna</a>
+            </div>
+            <div className={styles["menu-item"]}>
+              <a href="#">History</a>
+              <a href="#" className={styles["small"]}>Gardens</a>
+              <a href="#" className={styles["small"]}>Weather</a>
+            </div>
+            <div className={styles["menu-item"]}>
+              <a href="#">Things to Do</a>
+              <a href="#" className={styles["small"]}>Woodchop</a>
+              <a href="#" className={styles["small"]}>Bakeoff</a>
+            </div>
+            <div className={styles["menu-item"]}>
+              <a href="#">Community Pages</a>
+              <a href="#" className={styles["small"]}>Hepburn Shire</a>
+              <a href="#" className={styles["small"]}>Community Newsletter</a>
+              <a href="#" className={styles["small"]}>Committees and Working Group</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["history"]}>
+        <div className={styles["history-shadow"]}></div>
+        <img className={styles["history-bg"]} src="image-69.png" />
+        <div className={styles["history-content"]}>
+          <div className={styles["history-intro"]}>
+            <div>
+            <div className={styles["history-name"]}>HISTORY</div>
+            <div className={styles["history-context"]}>
+              <h1>Traditional Owner</h1>
+              <p>Djaara (Dja Dja Wurrung People) have lived on our traditional lands and cared for Djandak (Country) over many thousands of years. Djaara are the direct descendants of the First Peoples of Djandak, made up of many Clan groups who descend from our 18 Martinga kuli. Djandak is more than just a landscape, it is more than what is visible to the eye – it is a living entity, which holds the stories of creation and histories that cannot be erased.</p>
+            </div>
+            <div className={styles["more"]}>
+              <a href="#">Discover more -></a>
+            </div>
+            </div>
+
+            <div className={styles["history-about"]}>
+              <div className={styles["history-left"]}>
+                <img src="image-57.png" />
+              </div>
+
+              <div className={styles["history-right"]}>
+                <h1>
+                Settlement and Logging
+                </h1>
+                <p>Lyonville, once abundant with forest resources and fertile soil, attracted settlers in the 1860s, including timber workers and farmer James Lyon. The establishment of a school, railway, and various amenities led to the growth of a vibrant township centered around logging and agriculture</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["list"]}>
+        <div className={styles["item"]}>
+          <h1>Railway</h1>
+          <div className={styles["item-content"]}>
+            <p>Opened in 1880 and closed in 1978, now showcases a restored station building, platform, and railcar, offering a glimpse into its bygone era as a private residence.</p>
+            <div>
+            <img  src="image-20.png" />
+            </div>
+          </div>
+        </div>
+        <div className={styles["item"]}>
+          <h1>Wombat Forest</h1>
+          <div className={styles["item-content"]}>
+           <div className={styles["item-left"]}>
+            <p>Discover the captivating beauty of Wombat State Forest, a sprawling 70,000-hectare natural, offering diverse landscapes and rich biodiversity for nature enthusiasts and ecotourism explorers.</p>
+            <a href="#">Discover more -></a>
+           </div>
+            <div>
+            <img  src="image-56.png" />
+            </div>
+          </div>
+          
+        </div>
+        <div className={styles["item"]}>
+          <h1>Avenue of Honour</h1>
+          <div className={styles["item-content"]}>
+           <div className={styles["item-left"]}>
+            <p>Lyonville's Avenue of Honour, graced by solemn trees, honours those who've served with gratitude and reverence, embodying a lasting symbol of remembrance.</p>
+            <a href="#">Discover more -></a>
+           </div>
+            <div>
+            <img  src="image-55.png" />
+            </div>
+          </div>
+          
+        </div>
+        <div className={styles["item"]}>
+          <h1>Historical Walk and Map</h1>
+          <div className={styles["item-content"]}>
+           <div className={styles["item-left"]}>
+            <p>Discover Lyonville's captivating history through its charming Lyonville Hall, historic church, iconic Radio Springs Hotel, and more, offering a glimpse into the past with the Nolan's Creek sawmill walk and other fascinating sites!</p>
+            <a href="#">Discover more -></a>
+           </div>
+            <div>
+            <img  src="image-58.png" />
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+      <div className={styles["footer"]}>
+        <div className={styles["footer-content"]}>
+        <div className={styles["footer-content-left"]}>
+          <div className={styles["footer-menu"]}>
+            <h1>Lyonville</h1>
+            <div className={styles["footer-menu-content"]}>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                About Lyonville
+                </a>
+                <a href="#">
+                Getting there
+                </a>
+                <a href="#">
+                History
+                </a>
+                <a href="#">
+                Weather
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles["footer-menu"]}>
+            <h1>Calender Events</h1>
+            <div className={styles["footer-menu-content"]}>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                Open Garden Day
+                </a>
+                <a href="#">
+                Woodchop
+                </a>
+                <a href="#">
+                Bakeoff
+                </a>
+              </div>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                Café de Lyonville
+                </a>
+                <a href="#">
+                Radio Springs Hotel
+                </a>
+                <a href="#">
+                The Artful Meadows
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles["footer-menu"]}>
+            <h1>Community Pages</h1>
+            <div className={styles["footer-menu-content"]}>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                Community Pages
+                </a>
+                <a href="#">
+                Community Newsletter
+                </a>
+                <a href="#">
+                Rail Updates
+                </a>
+              </div>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                Hepburn Shire
+                </a>
+                <a href="#">
+                Fireguard
+                </a>
+                <a href="#">
+                Landcare
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles["footer-menu"]}>
+            <h1>Get in Contact</h1>
+            <div className={styles["footer-menu-content"]}>
+              <div className={styles["footer-menu-item"]}>
+                <a href="#">
+                Feedback and Ideas
+                </a>
+                <a href="#">
+                Facebook
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
+          <div className={styles["footer-content-right"]}>
+             <div className={styles["footer-logo-wrap"]}>
+             <img src="image-10.png" className={styles["footer-logo"]}/>
+             </div>
+             <div className={styles["icons"]}>
+             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20" fill="none">
+<path d="M8.51 3.32003H10.39V0.14003C9.47975 0.045377 8.56516 -0.00135428 7.65 2.98641e-05C4.93 2.98641e-05 3.07 1.66003 3.07 4.70003V7.32003H0V10.88H3.07V20H6.75V10.88H9.81L10.27 7.32003H6.75V5.05003C6.75 4.00003 7.03 3.32003 8.51 3.32003Z" fill="white"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M17.34 18.54C17.1027 18.54 16.8707 18.4696 16.6733 18.3378C16.476 18.2059 16.3222 18.0185 16.2313 17.7992C16.1405 17.5799 16.1168 17.3387 16.1631 17.1059C16.2094 16.8731 16.3236 16.6593 16.4915 16.4915C16.6593 16.3236 16.8731 16.2094 17.1059 16.1631C17.3387 16.1168 17.5799 16.1405 17.7992 16.2313C18.0185 16.3222 18.2059 16.476 18.3378 16.6733C18.4696 16.8707 18.54 17.1027 18.54 17.34C18.54 17.6583 18.4136 17.9635 18.1885 18.1885C17.9635 18.4136 17.6583 18.54 17.34 18.54ZM21.94 16.12C21.9206 16.9497 21.7652 17.7706 21.48 18.55C21.2257 19.2169 20.83 19.8207 20.32 20.32C19.8248 20.8326 19.2196 21.2258 18.55 21.47C17.7727 21.7638 16.9508 21.9228 16.12 21.94C15.06 22 14.72 22 12 22C9.28 22 8.94 22 7.88 21.94C7.04915 21.9228 6.22734 21.7638 5.45 21.47C4.78168 21.2234 4.17693 20.8304 3.68 20.32C3.16743 19.8248 2.77418 19.2196 2.53 18.55C2.23616 17.7727 2.07721 16.9508 2.06 16.12C2 15.06 2 14.72 2 12C2 9.28 2 8.94 2.06 7.88C2.07721 7.04915 2.23616 6.22734 2.53 5.45C2.77418 4.78044 3.16743 4.17518 3.68 3.68C4.17693 3.16956 4.78168 2.77665 5.45 2.53C6.22734 2.23616 7.04915 2.07721 7.88 2.06C8.94 2 9.28 2 12 2C14.72 2 15.06 2 16.12 2.06C16.9508 2.07721 17.7727 2.23616 18.55 2.53C19.2196 2.77418 19.8248 3.16743 20.32 3.68C20.8322 4.17742 21.2283 4.78181 21.48 5.45C21.7652 6.2294 21.9206 7.0503 21.94 7.88C21.94 8.94 22 9.28 22 12C22 14.72 22 15.06 21.94 16.12ZM20.14 8C20.1327 7.36523 20.0178 6.7363 19.8 6.14C19.6403 5.70481 19.3839 5.31158 19.05 4.99C18.7256 4.65949 18.3332 4.40357 17.9 4.24C17.3037 4.02225 16.6748 3.90728 16.04 3.9C15.04 3.85 14.67 3.84 12.04 3.84C9.41 3.84 9.04 3.84 8.04 3.9C7.38089 3.88766 6.72459 3.98915 6.1 4.2C5.68578 4.37192 5.31136 4.62721 5 4.95C4.66809 5.27125 4.41484 5.66481 4.26 6.1C4.01586 6.70485 3.88044 7.34806 3.86 8C3.86 9 3.8 9.37 3.8 12C3.8 14.63 3.8 15 3.86 16C3.86448 16.6489 3.98295 17.2921 4.21 17.9C4.38605 18.3221 4.65627 18.6983 5 19C5.30381 19.3438 5.67929 19.6169 6.1 19.8C6.70955 20.02 7.352 20.1349 8 20.14C9 20.14 9.37 20.2 12 20.2C14.63 20.2 15 20.2 16 20.14C16.6348 20.1327 17.2637 20.0178 17.86 19.8C18.3144 19.6313 18.7223 19.3572 19.05 19C19.3777 18.6928 19.6338 18.3173 19.8 17.9C20.0223 17.2911 20.1373 16.6482 20.14 16C20.19 15 20.2 14.63 20.2 12C20.2 9.37 20.19 9 20.14 8ZM12 17.13C10.9858 17.128 9.99496 16.8255 9.15265 16.2606C8.31035 15.6957 7.65438 14.8938 7.26763 13.9562C6.88089 13.0187 6.78072 11.9875 6.97979 10.9931C7.17886 9.99861 7.66824 9.08546 8.38608 8.36902C9.10392 7.65258 10.018 7.16498 11.0129 6.96785C12.0077 6.77072 13.0387 6.87289 13.9755 7.26146C14.9123 7.65003 15.7129 8.30756 16.2761 9.15097C16.8394 9.99437 17.14 10.9858 17.14 12C17.1413 12.6749 17.0092 13.3434 16.7512 13.967C16.4933 14.5907 16.1146 15.1572 15.6369 15.6339C15.1592 16.1107 14.5919 16.4883 13.9678 16.7451C13.3436 17.0018 12.6749 17.1326 12 17.13ZM12 8.67C11.3414 8.67 10.6976 8.8653 10.15 9.23121C9.60234 9.59711 9.17552 10.1172 8.92348 10.7257C8.67144 11.3341 8.6055 12.0037 8.73398 12.6497C8.86247 13.2956 9.17963 13.889 9.64533 14.3547C10.111 14.8204 10.7044 15.1375 11.3503 15.266C11.9963 15.3945 12.6659 15.3286 13.2743 15.0765C13.8828 14.8245 14.4029 14.3977 14.7688 13.85C15.1347 13.3024 15.33 12.6586 15.33 12C15.33 11.5627 15.2439 11.1297 15.0765 10.7257C14.9092 10.3216 14.6639 9.95455 14.3547 9.64533C14.0454 9.33611 13.6784 9.09083 13.2743 8.92348C12.8703 8.75613 12.4373 8.67 12 8.67Z" fill="white"/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M22 5.80021C21.2483 6.1263 20.4534 6.34187 19.64 6.44021C20.4982 5.92753 21.1413 5.12099 21.45 4.17021C20.6436 4.65027 19.7608 4.98851 18.84 5.17021C18.2245 4.50278 17.405 4.05851 16.5098 3.90706C15.6147 3.75562 14.6945 3.90557 13.8938 4.3334C13.093 4.76123 12.4569 5.44274 12.0852 6.27105C11.7135 7.09935 11.6273 8.0276 11.84 8.91021C10.2094 8.82774 8.61444 8.40316 7.15865 7.66407C5.70287 6.92498 4.41885 5.8879 3.39 4.62021C3.02914 5.25038 2.83952 5.96403 2.84 6.69021C2.83872 7.36459 3.00422 8.02883 3.32176 8.62377C3.63929 9.21872 4.09902 9.72592 4.66 10.1002C4.00798 10.0825 3.36989 9.90751 2.8 9.59021V9.64021C2.80489 10.5851 3.13599 11.4993 3.73731 12.2282C4.33864 12.957 5.17326 13.4559 6.1 13.6402C5.74326 13.7488 5.37287 13.806 5 13.8102C4.74189 13.8072 4.48442 13.7838 4.23 13.7402C4.49391 14.553 5.00462 15.2634 5.69107 15.7724C6.37753 16.2814 7.20558 16.5638 8.06 16.5802C6.6172 17.7155 4.83588 18.3351 3 18.3402C2.66574 18.3413 2.33174 18.3213 2 18.2802C3.87443 19.4905 6.05881 20.1329 8.29 20.1302C9.82969 20.1462 11.3571 19.8552 12.7831 19.2743C14.2091 18.6934 15.505 17.8341 16.5952 16.7467C17.6854 15.6593 18.548 14.3656 19.1326 12.9411C19.7172 11.5166 20.012 9.98994 20 8.45021C20 8.28021 20 8.10021 20 7.92021C20.7847 7.33502 21.4615 6.61763 22 5.80021Z" fill="white"/>
+</svg>
+             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
